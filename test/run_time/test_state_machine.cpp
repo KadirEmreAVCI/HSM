c1698d6fcbce7636306a0c47f67dc8df79209321
Kadir Emre Avcı <60631670+KadirEmreAVCI@users.noreply.github.com>
2026-03-12 17:58:16 +0300

    Remove pthread and thread-attribute support from active

--- test/run_time/test_state_machine.cpp
+++ test/run_time/test_state_machine.cpp
@@ -385,20 +385,6 @@ TEST(EAManagerRuntime, IBITAllowedWhileAttacking)
     }
 }
 
-TEST(EAManagerRuntime, ActiveAttributes_AreExposed)
-{
-    EAManager m(false, "ea_worker", 7, 8192);
-
-    EXPECT_EQ(m.thread_name(), "ea_worker");
-    EXPECT_EQ(m.thread_priority(), 7);
-    EXPECT_EQ(m.thread_stack_size(), 8192u);
-
-    const auto& attrs = m.get_thread_attributes();
-    EXPECT_EQ(attrs.name, "ea_worker");
-    EXPECT_EQ(attrs.priority, 7);
-    EXPECT_EQ(attrs.stack_size, 8192u);
-}
-
 TEST(EAManagerRuntime, ActiveStartStopLifecycle)
 {
     EAManager m;
